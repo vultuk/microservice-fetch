e0144d932c905824bc1081f59c04ca700e309672
Simon Skinner <vultuk28@gmail.com>
2021-04-08 13:27:33 +0100

    first commit

---
+++ src/Types/Fetch.ts
@@ -0,0 +1,18 @@
+type Fetch = {
+  get: <T = any>(uri: string, headers?: any[]) => (path?: string, headersOverride?: any[]) => Promise<T>;
+  post: <T = any, D = any>(
+    uri: string,
+    headers?: any[],
+  ) => (data: D, path?: string, headersOverride?: any[]) => Promise<T>;
+  put: <T = any, D = any>(
+    uri: string,
+    headers?: any[],
+  ) => (data: D, path?: string, headersOverride?: any[]) => Promise<T>;
+  patch: <T = any, D = any>(
+    uri: string,
+    headers?: any[],
+  ) => (data: Partial<D>, path?: string, headersOverride?: any[]) => Promise<T>;
+  delete: <T = any>(uri: string, headers?: any[]) => (path?: string, headersOverride?: any[]) => Promise<T>;
+};
+
+export default Fetch;
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import axios, {AxiosResponse} from 'axios';
+import {NextFunction, Request, Response} from 'express';
+
+import Fetch from './Types/Fetch';
+
+export * from './Types';
+
+declare global {
+  namespace Express {
+    interface Request {
+      fetch: Fetch;
+    }
+  }
+}
+
+export default () => (req: Request, res: Response, next: NextFunction) => {
+  req.fetch = {
+    get: <T = any>(uri: string, headers: any[] = []) => (path: string = '', headersOverride: any[] = []): Promise<T> =>
+      axios.get<T>(`${uri}/${path}`).then((response: AxiosResponse<T>) => response.data),
+    post: <T = any, D = any>(uri: string, headers: any[] = []) => (
+      data: D,
+      path: string = '',
+      headersOverride: any[] = [],
+    ): Promise<T> => axios.post<T>(`${uri}/${path}`, data).then((response: AxiosResponse<T>) => response.data),
+    patch: <T = any, D = any>(uri: string, headers: any[] = []) => (
+      data: D,
+      path: string = '',
+      headersOverride: any[] = [],
+    ): Promise<T> => axios.patch<T>(`${uri}/${path}`, data).then((response: AxiosResponse<T>) => response.data),
+    put: <T = any, D = any>(uri: string, headers: any[] = []) => (
+      data: D,
+      path: string = '',
+      headersOverride: any[] = [],
+    ): Promise<T> => axios.put<T>(`${uri}/${path}`, data).then((response: AxiosResponse<T>) => response.data),
+    delete: <T = any>(uri: string, headers: any[] = []) => (
+      path: string = '',
+      headersOverride: any[] = [],
+    ): Promise<T> => axios.delete<T>(`${uri}/${path}`).then((response: AxiosResponse<T>) => response.data),
+  };
+
+  next();
+};
