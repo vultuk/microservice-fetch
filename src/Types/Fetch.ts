63809bdbe7d8a26e934fbcce4d93764519b3cbea
Simon Skinner <vultuk28@gmail.com>
2021-05-26 14:39:40 +0100

    POST Header passing

--- src/Types/Fetch.ts
+++ src/Types/Fetch.ts
@@ -1,9 +1,6 @@
 type Fetch = {
   get: <T = any>(uri: string, headers?: any[]) => (path?: string, headersOverride?: any[]) => Promise<T>;
-  post: <T = any, D = any>(
-    uri: string,
-    headers?: any[],
-  ) => (data: D, path?: string, headersOverride?: any[]) => Promise<T>;
+  post: <T = any, D = any>(uri: string, headers?: any) => (data: D, path?: string, headersOverride?: any) => Promise<T>;
   put: <T = any, D = any>(
     uri: string,
     headers?: any[],
