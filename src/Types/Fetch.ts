db01496f947e35ab12e09d404502358cbc1941f9
Simon Skinner <vultuk28@gmail.com>
2021-06-10 20:50:47 +0100

    Fixing lack of headers

--- src/Types/Fetch.ts
+++ src/Types/Fetch.ts
@@ -1,15 +1,12 @@
 type Fetch = {
-  get: <T = any>(uri: string, headers?: any[]) => (path?: string, headersOverride?: any[]) => Promise<T>;
+  get: <T = any>(uri: string, headers?: any) => (path?: string, headersOverride?: any) => Promise<T>;
   post: <T = any, D = any>(uri: string, headers?: any) => (data: D, path?: string, headersOverride?: any) => Promise<T>;
-  put: <T = any, D = any>(
-    uri: string,
-    headers?: any[],
-  ) => (data: D, path?: string, headersOverride?: any[]) => Promise<T>;
+  put: <T = any, D = any>(uri: string, headers?: any) => (data: D, path?: string, headersOverride?: any) => Promise<T>;
   patch: <T = any, D = any>(
     uri: string,
-    headers?: any[],
-  ) => (data: Partial<D>, path?: string, headersOverride?: any[]) => Promise<T>;
-  delete: <T = any>(uri: string, headers?: any[]) => (path?: string, headersOverride?: any[]) => Promise<T>;
+    headers?: any,
+  ) => (data: Partial<D>, path?: string, headersOverride?: any) => Promise<T>;
+  delete: <T = any>(uri: string, headers?: any) => (path?: string, headersOverride?: any) => Promise<T>;
 };
 
 export default Fetch;
