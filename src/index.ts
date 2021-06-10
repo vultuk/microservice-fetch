337cfd36ddb43bb6b086ed0eb08e825c129a351b
Simon Skinner <vultuk28@gmail.com>
2021-06-10 20:44:34 +0100

    Fixing lack of headers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
-import { NextFunction, Request, Response } from 'express';
+import axios, {AxiosResponse} from 'axios';
+import {NextFunction, Request, Response} from 'express';
 
 import Fetch from './Types/Fetch';
 
@@ -15,8 +15,13 @@ declare global {
 
 export default () => (req: Request, res: Response, next: NextFunction) => {
   req.fetch = {
-    get: <T = any>(uri: string, headers: any[] = []) => (path: string = '', headersOverride: any[] = []): Promise<T> =>
-      axios.get<T>(`${uri}/${path}`).then((response: AxiosResponse<T>) => response.data),
+    get: <T = any>(uri: string, headers: any = {}) => (path: string = '', headersOverride: any = {}): Promise<T> =>
+      axios
+        .get<T>(
+          `${uri}/${path}`,
+          headers || headersOverride ? { headers: { ...headers, ...headersOverride } } : undefined,
+        )
+        .then((response: AxiosResponse<T>) => response.data),
     post: <T = any, D = any>(uri: string, headers: any = {}) => (
       data: D,
       path: string = '',
@@ -29,20 +34,32 @@ export default () => (req: Request, res: Response, next: NextFunction) => {
           headers || headersOverride ? { headers: { ...headers, ...headersOverride } } : undefined,
         )
         .then((response: AxiosResponse<T>) => response.data),
-    patch: <T = any, D = any>(uri: string, headers: any[] = []) => (
+    patch: <T = any, D = any>(uri: string, headers: any = {}) => (
       data: D,
       path: string = '',
-      headersOverride: any[] = [],
-    ): Promise<T> => axios.patch<T>(`${uri}/${path}`, data).then((response: AxiosResponse<T>) => response.data),
-    put: <T = any, D = any>(uri: string, headers: any[] = []) => (
+      headersOverride: any = {},
+    ): Promise<T> =>
+      axios
+        .patch<T>(`${uri}/${path}`, data, {
+          headers: headers || headersOverride ? { headers: { ...headers, ...headersOverride } } : undefined,
+        })
+        .then((response: AxiosResponse<T>) => response.data),
+    put: <T = any, D = any>(uri: string, headers: any = {}) => (
       data: D,
       path: string = '',
-      headersOverride: any[] = [],
-    ): Promise<T> => axios.put<T>(`${uri}/${path}`, data).then((response: AxiosResponse<T>) => response.data),
-    delete: <T = any>(uri: string, headers: any[] = []) => (
-      path: string = '',
-      headersOverride: any[] = [],
-    ): Promise<T> => axios.delete<T>(`${uri}/${path}`).then((response: AxiosResponse<T>) => response.data),
+      headersOverride: any = {},
+    ): Promise<T> =>
+      axios
+        .put<T>(`${uri}/${path}`, data, {
+          headers: headers || headersOverride ? { headers: { ...headers, ...headersOverride } } : undefined,
+        })
+        .then((response: AxiosResponse<T>) => response.data),
+    delete: <T = any>(uri: string, headers: any = {}) => (path: string = '', headersOverride: any = {}): Promise<T> =>
+      axios
+        .delete<T>(`${uri}/${path}`, {
+          headers: headers || headersOverride ? { headers: { ...headers, ...headersOverride } } : undefined,
+        })
+        .then((response: AxiosResponse<T>) => response.data),
   };
 
   next();
